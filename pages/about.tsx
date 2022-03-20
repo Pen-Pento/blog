162a4c7984637ca056d2ea1e0c9aa010b8dbc814
Pen-Pento <n.o-7010-summersound@ezweb.ne.jp>
2022-03-21 02:06:44 +0900

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,36 @@
+import Meta from '../components/meta';
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Container from "../components/container";
+
+const About = () => {
+  return (
+    <>
+      <Meta />
+      <Header />
+      <Container>
+        ROAD TO FULLSTACK<br />
+        <br />
+        初学時にフルスタックエンジニアになりたいと大口を叩いたばっかりに高いハードルに挑むことになった者です。日々ボス戦です。<br />
+        そんな日々の鬱憤を発散し、心の平穏を確保するべく<strong>ここをキャンプ地とする！！！</strong> となったわけです。<br />
+        ちなみに何を達成したらフルスタックエンジニアなのかは名言しません。ハードル高くなるので。プレッシャーなので。みんなの"フルスタック"があって良いじゃないのということです。<br />
+        <br />
+        主な経験<br />
+        PHP HTML CSS JS<br />
+        <br />
+        なのでブログにはこれ以外のことで学んだことメインにまとめていこうと思います。<br />
+        <br />
+        最後に、色々書きましたがただの雑記では書いていてテンションが上がらないので、本ブログの目的は<strong>自分なりの文章記載のメソッドを手に入れること</strong>とします。<br />
+        自分の考えをまとめたり、人に説明するのが苦手なのでちょうどいいでしょう。<br />
+        <br />
+        下記を進化させていくことになります。<br />
+        リンク<br />
+        <br />
+        なので全てリライト前提です。こっちの方が分かりやすいと思ったら書き直します。
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default About;
